bf28f7a1c21ec8ad879d965ac30ce6a00f501266
Facundo Guardia <101948163+facuguardia@users.noreply.github.com>
2024-05-19 22:00:29 -0300

    chore: Update npm dependencies and add AreaChartUsageExample component

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import Hero from './components/Hero';
 import Separator from './components/Separator';
 import About from './components/About';
 import Products from './components/Products';
+import { AreaChartUsageExample } from './pages/AreaChartUsageExample';
+
 
 function App() {
   return (
-    <div className=" bg-[#111827] w-screen h-screen">
-      <Navbar />
+    <div className=" bg-[#111827] w-[50%] h-screen">
+      {/* <Navbar />
       <Hero />
       <Separator />
       <About />
-      <Products />
+      <Products /> */}
+      <AreaChartUsageExample />
 
     </div>
   );
